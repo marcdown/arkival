b43d409c848480a0c9893d1d9e035b6012bfdbf0
Marc Brown <heymarcbrown@gmail.com>
2018-12-28 22:54:40 -0500

    File uploader UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import ipfs from './utils/ipfs';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  captureFile = (event) => {
+    const file = event.target.files[0];
+		console.log(file);
+  };
+
+  onSubmit = (event) => {
+    console.log('Upload');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="mb5">
+            <h1 className="f3 f1-m f-headline-l">arkival</h1>
+            <h2 className="f5 gray fw2 ttu tracked">Archive and timestamp your files using IPFS and Ethereum</h2>
         </header>
+        <div className="ph6 pv4 ba border-box">
+          <div>
+            <input type="file" name="file" id="file" onChange={this.captureFile}/>
+            <label className="f5 center no-underline black bg-animate pointer hover-bg-black hover-white inline-flex items-center pa3 ba border-box" htmlFor="file">
+              <svg width="20" height="17" viewBox="0 0 20 17" style={{fill: 'currentcolor'}}>
+                <path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/>
+              </svg>
+              <span className="pl1">Choose a file...</span>
+            </label>
+          </div>
+          <div>
+            <button className="f4 link pointer dim br2 ph3 pv2 mt4 dib white bg-near-black" onClick={this.onSubmit}>Upload</button>
+          </div>
+        </div>
       </div>
     );
   }
